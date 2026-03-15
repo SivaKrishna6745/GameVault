63984555362d3a1625c37319bb7ac2a99535ab60
SIVAKRISHNA <krishnasiva6745@gmail.com>
2026-03-15 22:08:47 +0530

    feat: Add initial store changes and integrate into layout.tsx using a client directive component StoreProvide.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
+import StoreProvider from '@/components/StoreProvider';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -29,8 +30,10 @@ export default function RootLayout({
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 <Navbar />
                 <div className="flex justify-around">
-                    <Sidebar />
-                    {children}
+                    <StoreProvider>
+                        <Sidebar />
+                        {children}
+                    </StoreProvider>
                 </div>
             </body>
         </html>
