0f84d4c981837eb6b7537f1c8c88d1dc093229a3
sivakrishna.m <sivakrishna.m@credera.com>
2026-03-16 17:13:48 +0530

    chore: Correct the prop received in GameCard

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -4,20 +4,23 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 interface GameProps {
-    game: Game;
+	game: Game;
 }
 
 function GameCard({ game }: GameProps) {
-    const { name, background_image, rating } = game;
-    const slugName = name.toLowerCase().replaceAll(' ', '-');
+	const { name, image_background, rating } = game;
+	const slugName = name.toLowerCase().replaceAll(' ', '-');
 
-    return (
-        <Link href={`/genre/${slugName}`} className="border border-slate-300 rounded-sm p-4 relative aspect-video">
-            <Image src={background_image} alt={name} fill className="object-cover" />
-            <h3 className="text-xl font-semibold">{name}</h3>
-            <h3 className="text-lg">{rating}</h3>
-        </Link>
-    );
+	return (
+		<Link
+			href={`/genre/${slugName}`}
+			className="border border-slate-300 rounded-sm p-4 relative aspect-video"
+		>
+			<Image src={image_background} alt={name} fill className="object-cover" />
+			<h3 className="text-xl font-semibold">{name}</h3>
+			<h3 className="text-lg">{rating}</h3>
+		</Link>
+	);
 }
 
 export default GameCard;
