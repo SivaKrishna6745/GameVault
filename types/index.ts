03a99fa22bca4974c7f91d6ae59f4f730a47f5a3
sivakrishna.m <sivakrishna.m@credera.com>
2026-03-17 17:24:35 +0530

    chore: Modify the type to match the RAWG API response

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,58 @@
+export type Platform = {
+	id: number;
+	name: string;
+	slug: string;
+	image_background: string;
+};
+
+type PlatformWrapper = {
+	platform: Platform;
+};
+
+type ParentPlatform = {
+	id: number;
+	name: string;
+	slug: string;
+};
+
+type ParentPlatformWrapper = {
+	parent_platform: ParentPlatform;
+};
+
+export type Genre = {
+	name: string;
+	slug: string;
+	image_background: string;
+};
+
+type Store = {
+	name: string;
+	slug: string;
+	image_background: string;
+};
+
+type StoreWrapper = {
+	id: number;
+	store: Store;
+};
+
+type Tag = {
+	id: number;
+	name: string;
+	slug: string;
+	image_background: string;
+};
+
 export type Game = {
 	id: number;
+	slug: string;
 	name: string;
 	background_image: string;
 	rating: number;
+	platforms: PlatformWrapper[];
+	parent_platforms: ParentPlatformWrapper[];
+	genres: Genre[];
+	stores: StoreWrapper[];
+	tags: Tag[];
 	user_rating?: number;
 };
