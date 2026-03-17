ebe7f07d066654f53c80352bdc1a0e7aae7b90d1
sivakrishna.m <sivakrishna.m@credera.com>
2026-03-17 17:25:08 +0530

    feat: Add utility file for all API calls

---
+++ lib/api.ts
@@ -0,0 +1,32 @@
+const API_KEY = process.env.RAWG_API_KEY;
+const BASE_URL = 'https://api.rawg.io/api';
+
+export const getAllGames = async () => {
+	const response = await fetch(`${BASE_URL}/games?key=${API_KEY}`);
+	if (!response.ok) throw new Error('Error while fetching all games!');
+	const gamesData = await response.json();
+	return gamesData.results;
+};
+
+export const getAllGenres = async () => {
+	const response = await fetch(`${BASE_URL}/genres?key=${API_KEY}`);
+	if (!response.ok) throw new Error('Error while fetching all genres!');
+	const genresData = await response.json();
+	return genresData.results;
+};
+
+export const getGamesByGenre = async (genre: string) => {
+	const response = await fetch(
+		`${BASE_URL}/games?key=${API_KEY}&genres=${genre}`,
+	);
+	if (!response.ok) throw new Error('Error while fetching games by genre!');
+	const gamesData = await response.json();
+	return gamesData.results;
+};
+
+export const getGame = async (slug: string) => {
+	const response = await fetch(`${BASE_URL}/games/${slug}?key=${API_KEY}`);
+	if (!response.ok) throw new Error('Error while fetching game data!');
+	const gamesData = await response.json();
+	return gamesData;
+};
