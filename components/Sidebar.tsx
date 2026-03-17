7b1895329aeb5b55ac4fe57c4c198dc5c3cba525
sivakrishna.m <sivakrishna.m@credera.com>
2026-03-17 19:10:04 +0530

    chore: Divide Sidebar into client and server components for better usability feat: Implement SidebarClient component for UI and client side code style: Make Sidebar UI improvements

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,26 +1,10 @@
 import { getAllGenres } from '@/lib/api';
-import { Genre } from '@/types';
-import Link from 'next/link';
-import React from 'react';
+import SidebarClient from './SidebarClient';
 
 async function Sidebar() {
 	const genres = await getAllGenres();
-	const buttonClassName =
-		'py-3 px-8 text-lg outline-none rounded-sm bg-slate-600 cursor-pointer hover:bg-slate-600/80 active:-translate-y-0.5 active:shadow-[3px_3px_5px_2px_rgba(256,256,256,0.8)]';
 
-	return (
-		<div className="flex flex-col gap-4 max-w-sm">
-			{genres.map((genre: Genre) => (
-				<Link
-					key={genre.slug}
-					href={`/${genre.name}`}
-					className={buttonClassName}
-				>
-					{genre.name}
-				</Link>
-			))}
-		</div>
-	);
+	return <SidebarClient genres={genres} />;
 }
 
 export default Sidebar;
---
+++ components/SidebarClient.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Genre } from '@/types';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+function SidebarClient({ genres }: { genres: Genre[] }) {
+	const buttonBaseClassName =
+		'py-3 px-6 text-md rounded-md whitespace-nowrap active:shadow-[2px_2px_3px_1px_rgba(256,256,256,0.8)] transition-all duration-200';
+	const buttonActiveClassName =
+		'bg-blue-500 text-white shadow-md scale-[1.02] cursor-not-allowed';
+	const buttonInactiveClassName =
+		'bg-slate-600 text-slate-200 hover:bg-slate-600 hover:text-white';
+	const pathname = usePathname().replace('/', '');
+
+	return (
+		<div className="flex flex-col gap-4 w-64 sticky top-0 h-full">
+			{genres.map((genre: Genre) => {
+				const isActive = pathname.toLowerCase() === genre.slug.toLowerCase();
+
+				return (
+					<Link
+						key={genre.slug}
+						href={`/${genre.slug}`}
+						className={`${buttonBaseClassName} ${isActive ? buttonActiveClassName : buttonInactiveClassName}`}
+					>
+						{genre.name}
+					</Link>
+				);
+			})}
+		</div>
+	);
+}
+
+export default SidebarClient;
